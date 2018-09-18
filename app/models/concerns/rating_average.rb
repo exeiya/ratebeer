57c16277e4e3f391ed946cb5880902f943a58657
exeiya <krista.lindholm@gmail.com>
2018-09-18 20:48:50 +0300

    Configurate RuboCop & improve syntax

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -1,8 +1,9 @@
 module RatingAverage
   extend ActiveSupport::Concern
 
-  def average_rating()
+  def average_rating
     return 0 if ratings.empty?
-      ratings.reduce(0.0) { |sum, rating| sum + rating.score } / ratings.size
+
+    ratings.reduce(0.0) { |sum, rating| sum + rating.score } / ratings.size
   end
 end
