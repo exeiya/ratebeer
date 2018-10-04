d131260ffea6debe1fcea20c0b3a839d53a808f1
exeiya <krista.lindholm@gmail.com>
2018-10-04 15:46:27 +0300

    Add view page to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,16 @@
 class PlacesController < ApplicationController
+  
   def index
+  end
+
+  def show
+    places = BeermappingApi.places_in(session[:last_place_search])
+    @place = places.find { |place| place.id == params[:id] }
   end
 
   def search
     @places = BeermappingApi.places_in(params[:city])
+    session[:last_place_search] = params[:city]
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -2,13 +2,6 @@ class BeermappingApi
   def self.places_in(city)
     city = city.downcase
 
-    #places = Rails.cache.read(city) 
-    #return places if places
-
-    #places = get_places_in(city)
-    #Rails.cache.write(city, places)
-    #places
-
     Rails.cache.fetch(city, expires_in: 1.week) { get_places_in(city) }
   end
 
