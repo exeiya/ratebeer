105bd54f5ce6ae1ebfe3e5f264e3b5564af9b6d6
exeiya <krista.lindholm@gmail.com>
2018-10-05 16:51:11 +0300

    Add weather to places with Apixu API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,11 +6,14 @@ class PlacesController < ApplicationController
   def show
     places = BeermappingApi.places_in(session[:last_place_search])
     @place = places.find { |place| place.id == params[:id] }
+    
+
   end
 
   def search
     @places = BeermappingApi.places_in(params[:city])
     session[:last_place_search] = params[:city]
+    @weather = ApixuApi.weather_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
---
+++ lib/apixu_api.rb
@@ -0,0 +1,12 @@
+class ApixuApi
+
+  def self.weather_in(city)
+    url = "https://api.apixu.com/v1/current.json?key=#{key}&q=#{city}"
+    response = HTTParty.get "#{url}"
+  end
+
+  def self.key
+    raise "APIXU_APIKEY env variable not defined" if ENV['APIXU_APIKEY'].nil?
+    ENV['APIXU_APIKEY']
+  end
+end
