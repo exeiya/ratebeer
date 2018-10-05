7365ee5e7da8b2c4a210ae6feb3d514cd58af63d
exeiya <krista.lindholm@gmail.com>
2018-10-05 17:18:31 +0300

    Fix tests

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -5,6 +5,13 @@ describe "Places" do
     allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
       [ Place.new( name:"Oljenkorsi", id: 1 ) ]
     )
+    allow(ApixuApi).to receive(:weather_in).with("kumpula").and_return(
+      {"location"=>{"name"=>"kumpula"},"current"=>
+      {"temp_c"=>22.0,
+       "condition"=>{"text"=>"Sunny", "icon"=>"//cdn.apixu.com/weather/64x64/day/113.png", "code"=>1000},
+       "wind_kph"=>6.8,
+       "wind_dir"=>"S"}}
+    )
 
     visit places_path
     fill_in('city', with: 'kumpula')
@@ -18,6 +25,13 @@ describe "Places" do
         Place.new( name:"Grilli", id: 2 ),
         Place.new( name:"Kolmas Baari", id: 3 ) ]
     )
+    allow(ApixuApi).to receive(:weather_in).with("kumpula").and_return(
+      {"location"=>{"name"=>"kumpula"},"current"=>
+      {"temp_c"=>22.0,
+       "condition"=>{"text"=>"Sunny", "icon"=>"//cdn.apixu.com/weather/64x64/day/113.png", "code"=>1000},
+       "wind_kph"=>6.8,
+       "wind_dir"=>"S"}}
+    )
 
     visit places_path
     fill_in('city', with: 'kumpula')
@@ -29,6 +43,7 @@ describe "Places" do
   end
   it "if no places are returned by the API, notification is shown at the page" do
     allow(BeermappingApi).to receive(:places_in).with("nowhere").and_return([])
+    allow(ApixuApi).to receive(:weather_in).with("nowhere").and_return([])
 
     visit places_path
     fill_in('city', with: 'nowhere')
