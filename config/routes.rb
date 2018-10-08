027a320c748a3ff0751c4720a1833eae3d82434d
exeiya <krista.lindholm@gmail.com>
2018-10-08 23:54:28 +0300

    Generate rest of the views and controller for Style

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :beers
   resources :breweries
+  resources :styles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'breweries#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
@@ -15,6 +16,4 @@ Rails.application.routes.draw do
   resources :places, only: [:index, :show]
   post 'places', to:'places#search'
 
-  #get 'styles', to: 'styles#index'
-  resources :styles, only: [:index, :show]
 end
