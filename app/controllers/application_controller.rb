e1c3b40ee6cc8b02914d22c58128930b54ca687c
exeiya <krista.lindholm@gmail.com>
2018-10-09 15:18:20 +0300

    Add admin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def ensure_that_signed_in
     redirect_to signin_path, notice: 'you should be signed in' if current_user.nil?
   end
+
+  def ensure_that_is_admin
+    redirect_to user_path(current_user), notice: "you don't have sufficient permissions" unless current_user.admin
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,11 @@ module ApplicationHelper
   def edit_and_destroy_buttons(item)
     unless current_user.nil?
       edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
-      del = link_to('Destroy', item, method: :delete,
-                                     data: { confirm: 'Are you sure?' },
-                                     class: "btn btn-danger")
+      if current_user.admin
+        del = link_to('Destroy', item, method: :delete,
+                                       data: { confirm: 'Are you sure?' },
+                                       class: "btn btn-danger")
+      end
       raw("#{edit} #{del}")
     end
   end
